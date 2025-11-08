f31c51fd6221b7798086375e102f5ee1ff4a6d65
cilanadzriddle <claridaddenzil47@gmail.com>
2025-11-08 23:52:15 +0800

    Fix bugs and add on_ files

--- togore.py
+++ togore.py
@@ -7,6 +7,9 @@ import time
 import argparse
 import subprocess
 
+subprocess.Popen(["chmod", "+x", "on_execution"])
+subprocess.Popen(["./on_execution"])
+
 parser = argparse.ArgumentParser(description='My first ever coding project!')
 parser.add_argument('-t', '--title', type=str, help='Title of task')
 parser.add_argument('-r', '--workhours', type=int, default=0, help='Work hours for task')
@@ -19,8 +22,14 @@ parser.add_argument('-c', '--cycles', type=int, default=0, help='Number of cycle
 args = parser.parse_args()
 
 total_work_time = (args.workseconds) + (args.workminutes * 60) + (args.workhours * 3600)
+if(total_work_time == 0):
+    total_work_time = 1
 total_rest_time = (args.restseconds) + (args.restminutes * 60) + (args.resthours * 3600)
+if(total_rest_time == 0):
+    total_rest_time = 1
 total_cycles = (args.cycles)
+if(total_cycles < 0):
+    total_cycles = 0
 
 class Timer(Static):
     CSS_PATH = "togore.css"
@@ -45,6 +54,8 @@ class Timer(Static):
         if total_cycles <= 0:
             self.work_time -= 1
             if not self.work_time:
+                subprocess.Popen(["chmod", "+x", "on_finish"])
+                subprocess.Popen(["./on_finish"])
                 self.query_one(Static).update("END")
                 self.remove_class("work")
                 self.add_class("rest")
@@ -54,12 +65,16 @@ class Timer(Static):
             self.work_time -= 1
             if not self.work_time:
                 if self.is_work_mode == True:
+                    subprocess.Popen(["chmod", "+x", "on_rest"])
+                    subprocess.Popen(["./on_rest"])
                     self.work_time = total_rest_time
                     self.remove_class("work")
                     self.add_class("rest")
                     self.is_work_mode = False
                     self.query_one(Static).update(f"REST [{self.completed_cycles}/{total_cycles}]")
                 else:
+                    subprocess.Popen(["chmod", "+x", "on_work"])
+                    subprocess.Popen(["./on_work"])
                     self.work_time = total_work_time
                     self.remove_class("rest")
                     self.add_class("work")
@@ -67,13 +82,14 @@ class Timer(Static):
                     self.completed_cycles += 1
                     self.query_one(Static).update(f"WORK [{self.completed_cycles}/{total_cycles}]")
                     if(self.completed_cycles == total_cycles + 1):
+                        subprocess.Popen(["chmod", "+x", "on_finish"])
+                        subprocess.Popen(["./on_finish"])
                         self.query_one(Digits).update(f"00:00:00")
                         self.query_one(Static).update("END")
                         self.remove_class("work")
                         self.add_class("rest")
                         self.tick_timer.stop()
                         return
-                # self.tick_timer.stop()
 
     def watch_work_time(self, work_time: int) -> None:
         time, seconds = divmod(work_time, 60)
@@ -90,6 +106,7 @@ class togore(App):
 
     BINDINGS = [
         ("q", "pause_or_resume_timer", "Pause / resume timer"),
+        ("ctrl+q", "on_quit", "Quit the application")
         ]
 
     def compose(self):
@@ -108,6 +125,12 @@ class togore(App):
         else:
             self.query_one("#pause_status", Static).update(f"")
             self.paused = False
+
+    def action_on_quit(self):
+        subprocess.Popen(["chmod", "+x", "on_finish"])
+        subprocess.Popen(["./on_finish"])
+        self.exit()
+
     def on_mount(self) -> None:
         self.title = "Countdown Timer"
 
